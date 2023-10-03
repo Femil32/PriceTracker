bf010427a963c931905d3bcffc60887082d08c1c
Femil32 <femilsavaliya555@gmail.com>
2023-10-04 00:26:34 +0530

    add vercel file

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -11,7 +11,7 @@ import {
 import { User } from "@/types";
 import { NextResponse } from "next/server";
 
-export const maxDuration = 300; // 5 minutes
+export const maxDuration = 300;
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
